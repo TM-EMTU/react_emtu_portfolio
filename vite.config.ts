5a09de557985f33d59189b2fe78ccebea4fe6602
Tanjil Mahmud Emtu <tmemtu123@gmail.com>
2025-05-31 13:28:16 +0600

    Update Vite base for custom domain deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react_emtu_portfolio-Public/', // <-- Add this line
+  base: '/', // <-- Change this line for custom domain
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
